ee7f57ff60f490b0662b719b5803161a5056bd34
Pujarani06052001 <103930803+Pujarani06052001@users.noreply.github.com>
2022-04-26 10:08:23 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,909 @@
+#a=[[1,2,3],[4,5,6],[7,8,9]]
+# sum=0
+# i=0
+# while i<len(a):
+# 	j=0
+# 	while j<len(a[i]):
+# 		sum=sum+(a[i][i])
+# 		j+=1
+# 	i+=1
+#print(sum)
+
+
+
+# a=['pooja ','rani', 'neha' ,'prajapati', 'soni']
+# b="and".join(a)
+# print(b,end='')
+
+
+
+
+# a=["my_name_is_pooja"]
+# i=0
+# while i<len(a):
+#     print(a[0][1])
+#     i+=1
+
+
+
+# a="my name is pooja"
+# i=0
+# c=0
+# b=""
+# while i<len(a):
+#     if a[i]==" ":
+#         c=c+1
+#     i=+1
+# print(c)
+
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# while a<=b:
+# 	i=1
+# 	while i<=10:
+# 		print(i,'=',i,'×',i*a)
+# 		i=i+1
+# 	a=a+1
+# 	print( )
+
+# a= [23, 14, 56, 12, 19, 9, 15, 25, 31, 42, 43]
+# i= 0
+# while i<len(a):
+# 	if a[i]%2==0:
+# 		print(a[i])
+# 	i+=1
+
+
+
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# l=[]
+# while i<len(a):
+#     b=a[i]==int
+#     l.append(b)
+#     print(b)
+# i+=1
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# l=[]
+# while i<len(a):
+#     j=0
+#     while j<len(a):
+#         if a[i]==int:
+#             l.append(int[i])
+#         j+=1
+#     i+=1
+# print(l)
+
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# for x in a:
+#     if int(x)==x:
+#         print(x)
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# l=[]
+# while i<len(a):
+#     b=a[i]==int
+#     l.append(b)
+# print(l)
+# i+=1
+
+
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# b=[]
+# while i<len(a):
+#     if a[i]==10:
+#         b.append(a)
+#     i+=1
+
+# print(b)
+
+
+# # a=[1,2,10,1.5,-7,2.6,8]
+# print(a[2])
+# i=0
+# l=[]
+# while i<len(a):
+#     if int(i)==i:
+#         l.append(i)
+#     i+=1
+# print(l)
+
+
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# while i<len(a):
+#         a.remove(10)
+#         print(a[i])
+#         i+=1
+
+
+#/////
+
+# # b=[]
+# # while i<le
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# l=[]
+# while i<len(a):
+#     a.remove(float[i])
+#     l.append(a[i])
+#     i+=1
+# print(l)
+
+
+
+
+# hume jo Value chahiye usko nikalna
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# b=[]
+# while i<len(a):
+#     if a[i]==10:
+#         b.append(a[i])
+    # i+=1
+# print(b)
+
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# while i<len(a):
+#     type=int(a[i])
+#     print(type)
+#     i+=1
+
+
+
+
+
+
+
+# date 25 march
+
+
+
+# find inger value
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# while i<len(a):
+#     if a[i]==int(a[i]):
+#         print(a[i])
+#     else:
+#         pass
+#     i=i+1
+
+
+# a=[1,2,10,1.5,-7,2.6,8]
+# i=0
+# b=[]
+# while i<len(a):
+#     if a[i]==10:
+#         b.append(a[i])
+#     i+=1
+# print(b
+
+# 
+
+
+
+# a=[1,2,3,0,0,4,6,0]
+# i=0
+# l=[]
+# l1=[]
+# while i<len(a):
+# 	if a[i]==0:
+# 		l.append(a[i])
+# 	if a[i]!=0:
+# 		l1.append(a[i])
+# 	i+=1
+# a=l1+l
+# print(a)
+
+
+
+
+# caunt
+
+
+# a=[1,2,3,0,0,4,6,0]
+# i=0
+# c=0
+# while i<len(a):
+# 	if a[i]==0:
+# 		c+=1
+# 	i+=1
+# print(c)
+
+
+# a= [2, -7, 5, -64, -14]
+# i=0
+# c=0
+# c2=0
+# while i<len(a):
+# 	if a[i]>0:
+# 		c+=1
+# 	else:
+# 		c2+=1
+# 	i+=1
+# print('pos',c)
+# print('nag',c2)
+
+ 
+
+
+# tables remove
+
+# mylist=[5,3,7,8,20,15,2,6,10,1]
+# for i in mylist:
+#     if (i%2==0):
+#         mylist.remove(i)
+# print(mylist)
+
+
+# def sum(a,a2):
+#     return a+a2
+# def sub(b,b2):
+#     return b
+
+# def fun(*a):
+#     print(a)
+# fun[4,9]
+
+# from binascii import b2a_qp
+
+
+# x=lambda a:a+b2
+
+# def sum(a):
+#     return(a)
+# print(sum("pooja"))
+
+
+
+# a=[1,2,3,4,5]
+# b=['one','two','three','four']
+# c=a+b
+# print(c)
+
+
+
+
+# a=[1,2,3,4,5]
+# b=['one','two','three']
+# l=[]
+# l.extend(a+b)
+# print(l)
+
+# li = [1,2,3,4,5]
+# while li:
+#     print(li.pop())
+
+
+
+# mylist=[5,3,7,8,20,15,2,6,10,1]
+# for i in mylist:
+#     if (i%2==0):
+#         mylist.remove(i)
+# print(mylist)
+
+
+# a=[1, 2, 3] 
+# b=len(a)
+# print(b)
+
+# a=[1, 2, 3] 
+# i=0
+# c=0
+# while i<len(a):
+#     if a[i]==a[i]:
+#         c+=1
+#     i+=1
+# print(c)
+
+
+
+
+
+# i=0
+# while i<5:
+#     if ["HI"]*5:
+#         print("hi",i)
+#     i+=1
+
+
+# Converts a tuple into list.
+
+
+# aTuple = (123, 'xyz', 'zara', 'abc');
+# aList = list(aTuple)
+# print(aList)
+
+
+
+# Appends object obj to list
+
+# aList = [123, 'xyz', 'zara', 'abc']
+# aList.append( 2009 )
+# print(aList)
+
+
+
+# Returns count of how many times obj occurs in list
+
+# aList = [123, 'xyz', 'zara', 'abc', 123];
+# print(aList.count(123))
+# print(aList.count('zara'))
+
+
+
+# Appends the contents of seq to list
+
+# aList = [123, 'xyz', 'zara', 'abc', 123];
+# bList = [2009, 'manni'];
+# aList.extend(bList)
+# print(aList)
+
+
+
+
+# Appends the contents of seq to list
+
+# aList = [123, 'xyz', 'zara', 'abc']
+# aList.insert( 3, 2009)
+# print(aList)
+
+
+# Appends the contents of seq to list
+
+# aList = [123, 'xyz', 'zara', 'abc']
+# print(aList.index( 'xyz' )) 
+# print(aList.index( 'zara' ))
+
+
+
+
+# Removes object obj from list
+
+# aList = [123, 'xyz', 'zara', 'abc']
+# print(aList.pop())
+# print(aList.pop(2))
+
+
+
+# Removes and returns last object or obj from list
+
+# aList = [123, 'xyz', 'zara', 'abc', 'xyz']
+# aList.remove('xyz')
+# print(aList)
+# aList.remove('abc')
+# print(aList)
+
+
+
+# Reverses objects of list in place
+
+# aList = [123, 'xyz', 'zara', 'abc', 'xyz']
+# aList.reverse()
+# print(aList)
+
+
+# list=[5,8,10,15,28]
+# list2=[5,6,3,2,10,28]
+# a=list
+# b=[]
+# i=0
+# while i<len(a):
+#     if a[i] not in b:
+#         b.append(a[i])
+#     i+=1
+# print(b)
+    
+# dout
+
+# list=[5,8,10,15,28]
+# list2=[5,6,3,2,10,28]
+# i=0
+# b=[]
+# while i<len(list):
+#     k=0
+#     while k<len(k[i]):
+#         if list==list2:
+
+
+    
+
+
+
+
+# SUNDAY
+
+# a=["geethu","neethu","geetha","geethu"]
+# i=0
+# b=[]
+# while i<len(a):
+#     j=0
+#     while j<len(a):
+#         if a[i]==a[j]and a[j]==a[i]:
+#             b.append(a[i][j])
+#             j+=1
+#         i+=1
+#     print(b)
+
+
+# from binascii import a2b_hqx
+
+
+
+
+
+
+
+
+
+# a="my name swathi am 20 years old"
+# s=""
+# i=0
+# c,c2=0,0
+# while i<len(a):
+#     if a[i]==" ":
+#         print(s,c2,c)
+#         c+=1
+#         c2=0
+#         s=""
+#     else:
+#         c2+=1
+#         s+=a[i]
+#     i+=1
+
+
+
+# a="my name swathi am 20 years old"
+# s=" "
+# i=0
+# c=0
+# c2=0
+# while i<len(a):
+#     if a[i]==" ":
+#         print(s,c2,c)
+#         c+=1
+#         c2=0
+#         s=" "
+#     else:
+#         c2+=1
+#         s+=a[i]
+#     i+=1
+
+# a=[2,4,8,9,10,4,2,1,8,9]
+# i=0
+# d=[]
+# while i<len(a):
+#     j=0
+#     c=0
+#     while j<len(a):
+#         if a[i]==a[j]:
+#             c+=1
+#         j+=1
+#     if a[i] not in d:
+#         d.append(a[i])
+#         print(a[i])
+#     i+=1
+
+
+
+# a=[2,4,8,9,10,4,2,1,8,9]
+# i=0
+# b=[]
+# while i<len(a):
+#     if a[i] not in b:
+#         b.append(a[i])
+#     i+=1
+# print(b)
+
+
+
+
+
+# a=[2,4,8,9,10,4,2,1,8,9]
+# b=[]
+# for i in a:
+#     n=a.count(i)
+#     if n>1:
+#         if b.count(i)==0:
+#             b.append(i)
+# print(b)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# res = []
+# for i in numbers:
+#     res.append(i * i)
+# print(res)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# res = [x * x for x in numbers]
+# print(res)
+
+
+
+
+
+# a= ["Mike", "", "Emma", "Kelly", "", "Brad"]
+# i=0
+# c=""
+# while i<len(a):
+#     if a[i]=="":
+#         a.remove(c)
+        
+#     i+=1
+# print(a)
+
+
+
+# wrong
+
+# a= [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# i=0
+# d=[]
+# while i<len(a):
+#     k=0
+#     while k<len(a[i]):
+#         d.append(a[i][k])
+#         k+=1
+#     i+=1
+# print(d)
+        
+
+# a=[[1,2,3],[4,5,6],[7,8,9]]
+# li=[]
+# i=0
+# while i<len(a):
+# 	j=0
+# 	while j<len(a[i]):
+#         while j<len(a[i]):
+# 		li.append(a[i][j])
+# 		j+=1
+# 	i+=1
+# print(li)
+
+
+
+
+# a="my name swathi am 20 years old"
+# b=""
+# c=0
+# c2=0
+# i=0
+# while i<len(a):
+#     if a[i]==" ":
+#         print(b,c,c2)
+#         c+=1
+#         c2=0
+#         b=""
+#     else:
+#         c2+=1
+#         b+=a[i]
+#     i+=1
+
+
+
+# def fun():
+#     return "hello"
+# print(fun())
+# print("hello")
+
+
+
+
+
+
+#c
+# variable = 'turkiye'
+# print(variable.count('t'))
+
+
+
+
+
+
+
+
+
+
+
+
+# TUESDAY
+
+
+# a="ayuxhi bparmar"
+# i=0
+# l=[]
+# c=0
+# c2=0
+# while i<len(a):
+    
+#     # n=a[i].split()
+#     l.append(a[i])
+#     if a[i]=="a":
+#         c+=1
+#     if a[i]=="r":
+#         c2+=1
+#     i+=1
+# print(l,c,c2)
+
+
+# n=["a","b","c","a","c","h","i","h","k","k"]
+# i=0
+# a=[]
+# while i<len(n):
+# 	j=0
+# 	c=0
+# 	while j<len(n):
+# 		if n[i]==n[j]:
+# 			c+=1
+# 		j+=1
+# 	if n[i] not in a:
+# 		a.append(n[i])
+# 		print([n[i],c])
+	# i+=1
+
+
+
+
+
+
+	
+
+
+
+# a="pooja rani"
+# b=a.split()
+# i=0
+# l=[]
+# while i<len(b):
+#     j=0
+#     while j<len(b[i]):
+#         l.append(b[i][j])
+#         j+=1
+#     i+=1
+# print(l)
+# n=l
+# i=0
+# a=[]
+# while i<len(n):
+# 	j=0
+# 	c=0
+# 	while j<len(n):
+# 		if n[i]==n[j]:
+# 			c+=1
+# 		j+=1
+# 	if n[i] not in a:
+# 		a.append(n[i])
+# 		print(n[i],c)
+# 	i+=1
+
+
+
+
+# n=["a","b","c","a","c","h","i","h","k","k"]
+# i=0
+# a=[]
+# while i<len(n):
+# 	j=0
+# 	c=0
+# 	while j<len(n):
+# 		if n[i]==n[j]:
+# 			c+=1
+# 		j+=1
+# 	if n[i] not in a:
+# 		a.append(n[i])
+# 		print([n[i],c])
+# 	i+=1
+
+
+    
+
+
+
+
+# a="pooja rani madhu shivani laxmni"
+# b=""
+# c=0
+# i=0
+# while i<len(a):
+#     if a[i]==" ":
+#         print(b,c)
+#         c+=1
+#         b=" "
+#     else:
+#         b+=a[i]
+#     i+=1
+
+
+# a="pooja rani madhu shivani laxmni"
+# b=" "
+# c=0
+# i=0
+# while i<len(a):
+#     if a[i]==" ":
+#         print(b,c)
+#         c+=1
+#         b=" "
+#     else:
+#         b+=a[i]
+#     i+=1
+
+
+
+
+
+# a="pooja rani madhu shivani laxmni "
+# b=" "
+# c=0
+# i=0
+# while i<len(a):
+#     if a[i]==" ":
+#         print(b,c)
+#         c+=1
+#         b=" "
+#     else:
+#         b+=a[i]
+#     i+=1
+
+
+# list=["pooja",5,3.6,"saudamini",2]
+# i=0
+# j=[]
+# sum=0
+# while i<len(list):
+# 	if type(list[i])==str:
+# 		j.append(list[i])
+# 	if type(list[i])==int or type(list[i])==float:
+# 		sum+=list[i]
+# 	i+=1
+# j.append(sum)
+# print(j)
+
+
+# l=[1,2,3,4]
+# i=-1
+# l2=[]
+# while i>(-5):
+# 	l2.append(l[i])
+# 	i-=1
+# print (l2)
+
+
+
+# a=[[1,2,3],[4,5,6],[7,8,9]]
+# li=[]
+# i=0
+# while i<len(a):
+# 	j=0
+# 	while j<len(a[i]):
+# 		li.append(a[i][j])
+# 		j+=1
+# 	i+=1
+# print(li)
+
+
+
+
+
+
+
+
+
+# WEDNESDAY
+
+# l=[3,4,[3,4,2],[3,2],3,5]
+# sum=0
+# i=0
+# while i<len(l):
+# 	if type(l[i]) == list:
+# 		j=0
+# 		while j<len(l[i]):
+# 			sum+=l[i][j]
+# 			j+=1
+# 	i+=1
+# print(sum)
+
+
+
+# l=[3,4,[3,4,2],[3,2],3,5]
+# sum,sum_int=0,0
+# i=0
+# while i<len(l):
+# 	if type(l[i]) == list:
+# 		j=0
+# 		while j<len(l[i]):
+# 			sum+=l[i][j]
+# 			j+=1
+#     else:
+#         sum_int+=l[i]
+#     i+=1
+# print(sum,sum_int)
+
+
+
+
+
+# n= ['pineapple', 'banana', 'watermelon', 'mango']
+# i= 0
+# while i<len(n):
+#     # a=n[i]
+#     print(len(n[i]))
+#     i+= 1
+
+
+# a= [5, 7, 8, 3, 4, 2, 9]
+# i=0
+# while i< len(a):
+#     if a[i]%2==0:
+#         print(a[i],"even")
+#     else:
+#         print(a[i],'odd')
+#     i+= 1
+
+
+
+# a= [21, 44, 35, 11]
+# for i in range(len(a)):
+#     print(i,a[i])
+
+
+
+
+
+
+# a= [21, 44, 35, 11]
+# i=0
+# c=0
+# while i<len(a):
+#     print(c,a[i])
+#     c+=1
+#     i+=1
+
+
+
+
+# a=["p",2,4,"pooja",5,8]
+# i=0
+# c=0
+# while i<len(a):
+#     print(c,a[i])
+#     c+=1
+#     i+=1
+
+
+# a=[1,2,56,3,2,2,8,9,8,9,6,4,10]
+# i=0
+# l=[]
+# while i<len(a):
+#     if a[i] not in l:
+#         l.append(a[i])
+#     i+=1
+# print(l)
+
+
+# l=[3,4,[3,4,2],[3,2],3,5]
+# sum=0
+# i=0
+# while i<len(l):
+# 	if type(l[i]) == list:
+# 		j=0
+# 		while j<len(l[i]):
+# 			sum+=l[i][j]
+# 			j+=1
+# 	i+=1
+# print(sum)
